f874e85af3c60a1b74025a866770265651bc75cc
support <sx3390115269@163.com>
2024-01-31 21:00:27 +0800

    完成编辑

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,62 @@
 <template>
-<div class="table-box">
-<!--标题-->
-  <div class="title">
-    <h2>最简单的 CRUD Demo</h2>
-  </div>
-<!-- query -->
-  <div class="query-box">
-    <el-input class="query-input" v-model="queryInput" placeholder="请输入姓名进行搜索" />
-    <div class="btn-list">
-      <el-button type="primary" @click="handleAdd">增加</el-button>
-      <el-button type="danger" @click="handleDelList" v-if="multipleSelection.length>0">多选删除</el-button>
+  <div class="table-box">
+    <!--标题-->
+    <div class="title">
+      <h2>最简单的 CRUD Demo</h2>
     </div>
-  </div>
-<!--  table-->
-  <el-table   ref="multipleTableRef"
-              :data="tableData"
-              style="width: 100%"
-              @selection-change="handleSelectionChange"
-              border>
-    <el-table-column type="selection" width="55" />
-    <el-table-column prop="name" label="姓名" width="120" />
-    <el-table-column prop="email" label="邮箱" width="120" />
-    <el-table-column prop="phone" label="电话" width="120" />
-    <el-table-column prop="state" label="状态" width="120" />
-    <el-table-column prop="address" label="地址" width="300" />
-    <el-table-column fixed="right" label="操作" width="120">
-      <template #default="scope">
-        <el-button link type="primary" size="small" @click="handleRowDel(scope.row)" style="color: #F56C6C">删除</el-button>
-        <el-button link type="primary" size="small">编辑</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-<!--  dialog-->
-  <el-dialog v-model="dialogFormVisible" :title="dialogType==='add'?'新增':'编辑'">
-    <el-form :model="tableForm">
-      <el-form-item label="姓名" :label-width="80">
-        <el-input v-model="tableForm.name" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="邮箱" :label-width="80">
-        <el-input v-model="tableForm.email" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="电话" :label-width="80">
-        <el-input v-model="tableForm.phone" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="状态" :label-width="80">
-        <el-input v-model="tableForm.state" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="地址" :label-width="80">
-        <el-input v-model="tableForm.address" autocomplete="off" />
-      </el-form-item>
-    </el-form>
-    <template #footer>
+    <!-- query -->
+    <div class="query-box">
+      <el-input class="query-input" v-model="queryInput" placeholder="请输入姓名进行搜索" />
+      <div class="btn-list">
+        <el-button type="primary" @click="handleAdd">增加</el-button>
+        <el-button type="danger" @click="handleDelList" v-if="multipleSelection.length>0">多选删除</el-button>
+      </div>
+    </div>
+    <!--  table-->
+    <el-table   ref="multipleTableRef"
+                :data="tableData"
+                style="width: 100%"
+                @selection-change="handleSelectionChange"
+                border>
+      <el-table-column type="selection" width="55" />
+      <el-table-column prop="name" label="姓名" width="120" />
+      <el-table-column prop="email" label="邮箱" width="120" />
+      <el-table-column prop="phone" label="电话" width="120" />
+      <el-table-column prop="state" label="状态" width="120" />
+      <el-table-column prop="address" label="地址" width="300" />
+      <el-table-column fixed="right" label="操作" width="120">
+        <template #default="scope">
+          <el-button link type="primary" size="small" @click="handleRowDel(scope.row)" style="color: #F56C6C">删除</el-button>
+          <el-button link type="primary" size="small" @click="handleEdit(scope.row)">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!--  dialog-->
+    <el-dialog v-model="dialogFormVisible" :title="dialogType==='add'?'新增':'编辑'">
+      <el-form :model="tableForm">
+        <el-form-item label="姓名" :label-width="80">
+          <el-input v-model="tableForm.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="邮箱" :label-width="80">
+          <el-input v-model="tableForm.email" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="电话" :label-width="80">
+          <el-input v-model="tableForm.phone" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="状态" :label-width="80">
+          <el-input v-model="tableForm.state" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="地址" :label-width="80">
+          <el-input v-model="tableForm.address" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
       <span class="dialog-footer">
         <el-button type="primary" @click="dialogConfirm">确定</el-button>
       </span>
-    </template>
-  </el-dialog>
-</div>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup>
@@ -127,17 +127,26 @@ const handleAdd=()=>{
   dialogFormVisible.value=true
   console.log(dialogFormVisible.value)
   tableForm.value={}
+  dialogType.value = 'add'
 }
 //确认
 const dialogConfirm=()=>{
   dialogFormVisible.value=false
-  //1.拿到数据
-  //2.添加到table
-  tableData.value.push({
-    id:(tableData.value.length+1).toString(),
-    ...tableForm.value
-  })
-  console.log(tableData);
+  //判断是编辑还是新增
+  if (dialogType.value === 'add'){
+    //1.拿到数据
+    //2.添加到table
+    tableData.value.push({
+      id:(tableData.value.length+1).toString(),
+      ...tableForm.value
+    })
+  }else {
+    //1.获取到当前的这条的索引
+    let index =tableData.value.findIndex(item => item.id === tableForm.value.id)
+    tableData.value[index]=tableForm.value
+    //2.替换当前索引值对应的数据
+  }
+
 }
 //删除一条
 const handleRowDel=({id})=>{
@@ -154,6 +163,14 @@ const handleDelList=()=>{
     handleRowDel({id})
   })
   multipleSelection.value=[]
+}
+//编辑
+const handleEdit=(row)=>{
+  dialogFormVisible.value=true
+  dialogType.value = 'edit'
+  tableForm.value = {...row}
+
+
 }
 </script>
 
