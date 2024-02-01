1bed9bae82e77e2a52269486609ab1a96a8de9a0
support <sx3390115269@163.com>
2024-02-01 22:27:44 +0800

    上传配置文件

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    // port: 8080 //指定端口号
+
+    proxy: {
+      '/user': {
+        target: "http://localhost:3001/"
+      }
+    }
+  },
+
+  base: './', //打包相对路径
 })
